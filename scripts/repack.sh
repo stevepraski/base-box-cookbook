469177a8bdddab925a15877e5d495c7939ae4747
stevepraski <stevenpraski@gmail.com>
2015-05-24 14:24:17 -0700

    add ubuntu support

--- scripts/repack.sh
+++ scripts/repack.sh
@@ -6,7 +6,7 @@ if [ ! -n "$1" ]; then
   exit $E_BADARGS
 fi  
 
-SUPPORTED_BOXES=("centos-6.5")
+SUPPORTED_BOXES=("centos-6.5" "ubuntu-12.04")
 
 for box in ${SUPPORTED_BOXES[@]}; do
   if [[ $box =~ $1 ]]; then
@@ -22,13 +22,22 @@ if [[ ! $BOX_NAME ]]; then
 fi
 
 PACK_SCRIPT="repack_${BOX_NAME%-*}.json"
+OPS_BOX_NAME="opscode-${BOX_NAME}"
 
-if [ ! -d "$HOME/.vagrant.d/boxes/opscode-$BOX_NAME" ]; then
-  vagrant box add opscode-centos-6.5 http://opscode-vm-bento.s3.amazonaws.com/vagrant/virtualbox/opscode_${BOX_NAME}_chef-provisionerless.box
+if [ ! -d "${HOME}/.vagrant.d/boxes/${OPS_BOX_NAME}" ]; then
+  vagrant box add ${OPS_BOX_NAME} http://opscode-vm-bento.s3.amazonaws.com/vagrant/virtualbox/opscode_${BOX_NAME}_chef-provisionerless.box
 fi
 
-rm -fr berks-cookbooks
-berks vendor -b ../Berksfile
+# Cleanup existing vendoring
+if [ -d "berks-cookbooks" ]; then
+  rm -r berks-cookbooks
+fi 
 
-packer validate -var "origin_box_name=opscode-${BOX_NAME}" ${PACK_SCRIPT}
-packer build -var "origin_box_name=opscode-${BOX_NAME}" ${PACK_SCRIPT}
+# Cleanup previous run
+if [ -d "output-virtualbox-ovf" ]; then
+  rm -r output-virtualbox-ovf
+fi
+
+berks vendor -b ../Berksfile
+packer validate -var "origin_box_name=${OPS_BOX_NAME}" ${PACK_SCRIPT}
+packer build -var "origin_box_name=${OPS_BOX_NAME}" ${PACK_SCRIPT}
